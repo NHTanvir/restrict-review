901f6b979c90921343b55248a0846f23f2c03025
NHTanvir <36070006+NHTanvir@users.noreply.github.com>
2025-09-20 19:58:57 +0600

    my feedback optimized

--- views/tabs/my-feedbacks.php
+++ views/tabs/my-feedbacks.php
@@ -3,91 +3,89 @@ global $wpdb;
 
 $current_user_id = get_current_user_id();
 
-if ( $current_user_id == 0 ) {
-    echo '<p>You must be logged in to view your reviews.</p>';
-    return;
+if ( 0 === $current_user_id ) {
+	echo '<p>You must be logged in to view your reviews.</p>';
+	return;
 }
 
-$table_name = $wpdb->prefix . 'jet_reviews'; 
+$table_name = $wpdb->prefix . 'jet_reviews';
 
-$query = "
-    SELECT *
-    FROM $table_name
-    WHERE author = %d
-    ORDER BY date DESC
+$query   = "
+	SELECT *
+	FROM $table_name
+	WHERE author = %d
+	ORDER BY date DESC
 ";
-
 $results = $wpdb->get_results( $wpdb->prepare( $query, $current_user_id ) );
 
 if ( empty( $results ) ) {
-    echo '<p>No reviews found.</p>';
-    return;
+	echo '<p>No reviews found.</p>';
+	return;
 }
 
 foreach ( $results as $row ) {
-            
-    $user_id    = get_post_meta( $row->post_id, 'user_id', true );
-    $user       = get_user_by('ID', $user_id);
-    $username   = $user->user_login;
-    $user_url   = get_permalink( $row->post_id );
-    
-    ?>
-<table class="feedback-table" border="1" cellpadding="10" cellspacing="0">
-    <tbody>
-        <tr>
-            <td>User ID</td>
-            <td>
-                <?php if ($user_url): ?>
-                    <a href="<?php echo esc_url($user_url); ?>" target="_blank">
-                        <?php echo esc_html($username); ?>
-                    </a>
-                <?php else: ?>
-                    <?php echo esc_html($username); ?>
-                <?php endif; ?>
-            </td>
-        </tr>
-        <tr>
-            <td>Title</td>
-            <td><?php echo esc_html( $row->title ); ?></td>
-        </tr>
-        <tr>
-            <td>Content</td>
-            <td><?php echo esc_html( $row->content ); ?></td>
-        </tr>
-        <tr>
-            <td>Rating</td>
-            <td>
-                <div class="jet-reviews-stars jet-reviews-stars--adjuster restrict-reviews-stars">
-                                <?php 
-                                $full_stars = floor($row->rating / 20);
-                                $empty_stars = 5 - $full_stars;
-                                
-                                for ($i = 0; $i < $full_stars; $i++) {
-                                    echo '
-                                    <div class="jet-reviews-star">
-                                        <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg" class="e-font-icon-svg e-fas-star" style="width: 10px; height: 10px; fill: #FFC400;">
-                                            <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
-                                        </svg>
-                                    </div>';
-                                }
+	$user_id  = get_post_meta( $row->post_id, 'user_id', true );
+	$user     = get_user_by( 'ID', $user_id );
+	$username = $user ? $user->user_login : '';
+	$user_url = get_permalink( $row->post_id );
+	?>
+	<table class="feedback-table" border="1" cellpadding="10" cellspacing="0">
+		<tbody>
+			<tr>
+				<td>User ID</td>
+				<td>
+					<?php if ( $user_url ) : ?>
+						<a href="<?php echo esc_url( $user_url ); ?>" target="_blank">
+							<?php echo esc_html( $username ); ?>
+						</a>
+					<?php else : ?>
+						<?php echo esc_html( $username ); ?>
+					<?php endif; ?>
+				</td>
+			</tr>
+			<tr>
+				<td>Title</td>
+				<td><?php echo esc_html( $row->title ); ?></td>
+			</tr>
+			<tr>
+				<td>Content</td>
+				<td><?php echo esc_html( $row->content ); ?></td>
+			</tr>
+			<tr>
+				<td>Rating</td>
+				<td>
+					<div class="jet-reviews-stars jet-reviews-stars--adjuster restrict-reviews-stars">
+						<?php
+						$full_stars  = floor( $row->rating / 20 );
+						$empty_stars = 5 - $full_stars;
 
-                                // Output empty stars
-                                for ($i = 0; $i < $empty_stars; $i++) {
-                                    echo '
-                                    <div class="jet-reviews-star">
-                                        <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg" class="e-font-icon-svg e-fas-star" style="width: 10px; height: 10px; fill: #FFC400;">
-                                            <path d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
-                                        </svg>
-                                    </div>';
-                                }
-                                ?>
-                            </div>
-                </td>
-        </tr>
-        <tr>
-            <td>Date</td>
-            <td><?php echo esc_html( $row->date ); ?></td>
-        </tr>
-    </tbody>
-</table>
-<?php } ?>
+						for ( $i = 0; $i < $full_stars; $i++ ) {
+							echo '
+							<div class="jet-reviews-star">
+								<svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg" class="e-font-icon-svg e-fas-star" style="width: 10px; height: 10px; fill: #FFC400;">
+									<path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
+								</svg>
+							</div>';
+						}
+
+						for ( $i = 0; $i < $empty_stars; $i++ ) {
+							echo '
+							<div class="jet-reviews-star">
+								<svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg" class="e-font-icon-svg e-fas-star" style="width: 10px; height: 10px; fill: #FFC400;">
+									<path d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
+								</svg>
+							</div>';
+						}
+						?>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td>Date</td>
+				<td><?php echo esc_html( $row->date ); ?></td>
+			</tr>
+		</tbody>
+	</table>
+	<?php
+}
+?>
